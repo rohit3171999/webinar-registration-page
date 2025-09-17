f23f065cfbf9d58f628b80769e711eacea20f622
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 12:55:14 +0530

    add functional component in TestimonialsSection.jsx

---
+++ src/components/TestimonialsSection.jsx
@@ -0,0 +1,24 @@
+/*
+* Description:
+Displays quotes or reviews from beta testers/early adopters.
+
+Purpose:
+Provide social proof and credibility.
+* */
+export default function TestiomonialsSection(){
+    return (
+        // <section> wrapper
+        <section>
+            {/*<h2> section title (e.g., “What Teams Are Saying”)*/}
+            <h2></h2>
+            {/*<div> container for testimonial cards*/}
+            <div></div>
+            {/*<blockquote> testimonial text*/}
+
+            {/*    <p> user name + role*/}
+
+            {/*        <img> (optional: avatar of person/team logo)*/}
+
+        </section>
+    );
+}
